a922b7b2f3483c61aafb13b9f8d982464826e1e0
Dexter84 <fenixzaec84@gmail.com>
2024-11-24 16:16:16 +0700

    lesson21 fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
-//import validation from './modules/validation'
+
 
 timer('27 november 2024')
 menu()
 modal()
-//validation()
