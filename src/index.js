cdb4890d58eff7f5393642aaadebaeeabe481009
Dexter84 <fenixzaec84@gmail.com>
2024-11-27 20:35:44 +0700

    lesson24 complete

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import slider from './modules/slider'
+import calc from './modules/calc'
 
-timer('21 november 2024')
+timer('30 november 2024')
 menu()
 modal()
 slider()
+calc()
