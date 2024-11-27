c6554d97c68f4aeacaa0e7da691a61171baea6cf
Dexter84 <fenixzaec84@gmail.com>
2024-11-27 17:45:01 +0700

    lesson23 auestion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
-import validation from './modules/validation'
+import slider from './modules/slider'
 
 timer('21 november 2024')
 menu()
 modal()
-validation()
+slider()
