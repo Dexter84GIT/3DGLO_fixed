f1fbdcf04de5a4268acf9435229aa68fd1b20de0
Dexter84 <fenixzaec84@gmail.com>
2024-11-28 11:18:20 +0700

    lesson23 broken

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 import slider from './modules/slider'
+import tabs from './modules/tabs'
 import calc from './modules/calc'
 
 timer('30 november 2024')
 menu()
 modal()
 slider()
+tabs()
 calc()
