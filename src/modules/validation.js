384a4e7f3926e235b06cbc93b402cf6edab5f71f
Dexter84 <fenixzaec84@gmail.com>
2024-11-30 20:49:51 +0700

    lesson21 complete fix

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,56 +1,3 @@
-//const validation = () => {
-//    const textInput = document.querySelectorAll('input[type="text"]')
-//    const textarea = document.querySelector('.mess')
-//    const form = document.querySelectorAll('form')
-//
-//form.forEach(form => {
-//    form.addEventListener('submit', (e) => {
-//        e.preventDefault()
-//
-//        textInput.forEach(input => {
-//            input.addEventListener('input', (e) => {
-//                if (e.value != (/[^а-яА-Я]/g.test(e.value))) {
-//                    console.log('Херня какая-то');
-//                } else {
-//                    console.log('Да всё ок вроде');
-//                }
-//            })
-//        }) 
-//    })
-//})
-//    form.addEventListener('submit', (e) => {
-//       e.preventDefault()
-//        let isError = false
-//    
-//        if (!/[^а-яА-Я]/g.test(textInput.value) && textInput.value !== '') {
-//            alert('Только кириллица')
-//        } else {
-//            isError = true
-//        }
-//    
-//        if (!/[^\d]/g.test(numberInput.value) && numberInput.value !== '') {
-//            alert('Только цифры')
-//        } else {
-//            isError = true
-//        }
-//    
-//        if (!isError) {
-//            alert('Send')
-//        }
-
-
- //   })
-
-
-
-//    console.log(textInput);
-//    console.log(textarea);
-//    console.log(form);
-//    
-//}
-//
-//export default validation
-
 const validate = () => {
     const form = document.querySelectorAll('form')
     const textInput = document.querySelectorAll('input[type="text"]')
@@ -66,7 +13,7 @@ const validate = () => {
             const regExp = /[a-zA-Z]/gi
             const testEmail = /[^a-zA-Z0-9\@\-\_\.\!\~\*\']/gi
             const testPhone = /[^0-9\(\)\-]/gi
-        
+
             textInput.forEach(input => {
                 input.addEventListener('input', (e) => {
                     if (regExp.test(value) && value !== '') {
